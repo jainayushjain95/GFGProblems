0f3d2a12a965a5d15cdcc81f8ec5aa36d7ec7017
Ayush jain <jain.ayush@naukri.com>
2020-02-24 19:07:04 +0530

    BIT magic problems

--- GFG/src/bitmagic/Problem9.java
+++ GFG/src/bitmagic/Problem9.java
@@ -3,8 +3,31 @@ package bitmagic;
 public class Problem9 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int m = 52, n = 4;
+		System.out.println(getRightMostDifferentBitPosition(m, n));
+	}
+	
+	public static int getRightMostDifferentBitPosition(int m, int n) {
+		int rightMostDifferentBitPosition = -1;
+		if(m != n) {
+			rightMostDifferentBitPosition = 0;
+			int xor = m ^ n;
+			rightMostDifferentBitPosition = getFirstSetBitFromRight(xor);
+		}
+		return rightMostDifferentBitPosition;
+	}
+	
+	public static int getFirstSetBitFromRight(int n) {
+		int ans = 1;
+		while(n > 0) {
+			if((n & 1) == 0) {
+				ans++;
+			} else {
+				break;
+			}
+			n = n >> 1;
+		}
+		return ans;
 	}
 
 }
