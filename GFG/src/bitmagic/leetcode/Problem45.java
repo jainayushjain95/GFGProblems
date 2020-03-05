5a9e03f6130a30edd5e06850175f20167ac3875e
Ayush jain <jain.ayush@naukri.com>
2020-03-05 19:47:27 +0530

    Number Complement problem

--- GFG/src/bitmagic/leetcode/Problem45.java
+++ GFG/src/bitmagic/leetcode/Problem45.java
@@ -3,29 +3,26 @@ package bitmagic.leetcode;
 public class Problem45 {
 
 	public static void main(String[] args) {
-		System.out.println(getNosComplement(1));
+		//		System.out.println(1 << 2);
+		System.out.println(getNosComplement(5));
 	}
 
 	public static int getNosComplement(int n) {
 		if(n == 0) {
 			return 1;
 		}
-		int temp = n;
+		return (getNoWithAllSetBits(n)) ^ n;
+	}
+
+	//and in that no, no of bits is same as in n
+	//ex: n = 01101 --> output = 11111
+	public static int getNoWithAllSetBits(int n) {
 		int bitsCount = noOfBitsReqd(n);
-		for(int i = 0;i < bitsCount; i++) {
-			int lastBit = temp & 1;
-			if(lastBit == 0) {
-				n = n + (int)Math.pow(2, i);
-			} else {
-				n = n - (int)Math.pow(2, i);
-			}
-			temp = temp >> 1;
-		}
-		return n;
+		return (1 << bitsCount) - 1;
 	}
 	
 	public static int noOfBitsReqd(int n) {
 		return 1 + (int)Math.floor(Math.log(n)/Math.log(2));
 	}
-	
+
 }
