4d52eacc147be494fabef3668e02dcd696586f96
Ayush jain <jain.ayush@naukri.com>
2020-04-15 09:00:15 +0530

    Linked List problems

--- GFG/src/linked/lists/LinkedList.java
+++ GFG/src/linked/lists/LinkedList.java
@@ -4,18 +4,18 @@ public class LinkedList {
 
 	Node head;
 	int sizeOfLinkedList;
-	
+
 	public LinkedList() {
 		sizeOfLinkedList = 0;
 	}
-	
+
 	public void insertAtBegining(int data) {
 		Node newNode = new Node(data);
 		newNode.next = head;
 		head = newNode;
 		sizeOfLinkedList++;
 	}
-	
+
 	public void insertAtTheEnd(int data) {
 		Node newNode = new Node(data);
 		Node temp = head;
@@ -27,10 +27,10 @@ public class LinkedList {
 			}
 			temp.next = newNode;
 		}
-		
+
 		sizeOfLinkedList++;
 	}
-	
+
 	public int findTheMiddleOfLinkedList() {
 		int midData = -1;
 		Node slow = head;
@@ -44,7 +44,7 @@ public class LinkedList {
 		}
 		return midData;
 	}
-	
+
 	public int findNthNodeFromLinkedList(int n) {
 		int nthNodeDataFromEnd = -1;
 		Node first = head, second = head;
@@ -61,6 +61,129 @@ public class LinkedList {
 		}
 		return nthNodeDataFromEnd;
 	}
+
+	public Node linkedListReversalIterative() {
+		Node current = head, prev = null, next = null;
+		while(current != null) {
+			next = current.next;
+			current.next = prev;
+			prev = current;
+			current = next;
+		}
+		return prev;
+	}
+	
+	public Node linkedListReversalRecursiveOne(Node head) {
+		if(head == null || head.next == null) {
+			return head;
+		}
+		Node smallerHead = linkedListReversalRecursiveOne(head.next);
+		head.next.next = head;
+		head.next = null;
+		return smallerHead;
+	}
+
+	public Node linkedListReversalRecursiveTwo(Node current, Node previous) {
+		if(current == null) {
+			return previous;
+		}
+		Node next = current.next;
+		current.next = previous;
+		return linkedListReversalRecursiveTwo(next, current);
+	}
+
+	/*
+	 * reverse a linked list in groups of size k
+	 */
+
+	public Node reverseLinkedListInGroups(Node head, int k) {
+		Node current = head, prev = null, next = null;
+		int count = k;
+		while(count != 0 && current != null) {
+			next = current.next;
+			current.next = prev;
+			prev = current;
+			current = next;
+			count--;
+		}
+		if(current != null) {
+			Node tempHead = reverseLinkedListInGroups(current, k);
+			head.next = tempHead;
+		}
+		return prev;
+	}
+
+	
+	public Node segrateOddsAndEvens() {
+		Node evenStart = null, evenEnd = null, oddStart = null, oddEnd = null;
+		Node temp = head;
+		Node output = null;
+		while(temp != null) {
+			int data = temp.data;
+			if(data % 2 == 0) {
+				if(evenStart == null) {
+					evenStart = temp;
+					evenEnd = evenStart;
+				} else {
+					evenEnd.next = temp;
+					evenEnd = temp;
+				}
+			} else {
+				if(oddStart == null) {
+					oddStart = temp;
+					oddEnd = oddStart;
+				} else {
+					oddEnd.next = temp;
+					oddEnd = temp;
+				}
+			}
+			temp = temp.next;
+		}
+		if(evenStart == null || oddStart == null) {
+			output = head;
+		} else {
+			evenEnd.next = oddStart;
+			oddEnd.next = null;
+			output = evenStart;
+		}
+		return output;
+	}
+	
+	
+	public Node mergeTwoSortedLinkedListsInPlace(Node firstHead, Node secondHead) {
+		Node temp1 = firstHead, temp2 = secondHead;
+		Node outputHead = null;
+		if(temp1 == null) {
+			return temp2;
+		} else if(temp2 == null) {
+			return temp1;
+		}
+		if(temp1.data < temp2.data) {
+			outputHead = temp1;
+		} else {
+			outputHead = temp2;
+		}
+		while(temp1 != null && temp2 != null) {
+			if(temp1.data < temp2.data) {
+				while(temp1.next != null && temp1.next.data < temp2.data) {
+					temp1 = temp1.next;
+				}
+				Node temp = temp1.next;
+				temp1.next = temp2;
+				temp1 = temp;
+			} else {
+				while(temp2.next != null && temp2.next.data < temp1.data) {
+					temp2 = temp2.next;
+				}
+				Node temp = temp2.next;
+				temp2.next = temp1;
+				temp2 = temp;
+			}
+			
+		}
+		
+		return outputHead;
+	}
 	
 	public void printLinkedList() {
 		Node temp = head;
@@ -69,5 +192,13 @@ public class LinkedList {
 			temp = temp.next;
 		}
 	}
-	
+
+	public void printLinkedList(Node head) {
+		Node temp = head;
+		while(temp != null) {
+			System.out.println(temp.data);
+			temp = temp.next;
+		}
+	}
+
 }
--- GFG/src/linked/lists/LinkedListDriver.java
+++ GFG/src/linked/lists/LinkedListDriver.java
@@ -3,11 +3,24 @@ package linked.lists;
 public class LinkedListDriver {
 
 	public static void main(String[] args) {
-		LinkedList linkedList = new LinkedList();
-		for(int i = 10; i <= 60; i = i + 10) {
-			linkedList.insertAtTheEnd(i);
+//		LinkedList linkedList = new LinkedList();
+//		for(int i = 2; i <= 10; i = i + 2) {
+//			linkedList.insertAtTheEnd(i);
+//		}
+//		Node newHead = linkedList.segrateOddsAndEvens();
+//		linkedList.printLinkedList(newHead);
+		LinkedList ll1 = new LinkedList();
+		for(int i = 1;i <= 5; i++) {
+			ll1.insertAtTheEnd(i);
 		}
-		System.out.println(linkedList.findNthNodeFromLinkedList(22));
+		
+		LinkedList ll2 = new LinkedList();
+		for(int i = 10;i <= 15; i++) {
+			ll2.insertAtTheEnd(i);
+		}
+		
+		Node mergedLL = ll1.mergeTwoSortedLinkedListsInPlace(ll1.head, ll2.head);
+		ll1.printLinkedList(mergedLL);
 	}
 
 }
