1e7059fa5031457566408391ef4071f3c9f30e73
Ayush Jain <jainayushjain95@gmail.com>
2020-09-15 10:17:25 +0530

    bt

--- ReReiterate/src/trees/binarytrees/BinaryTree.java
+++ ReReiterate/src/trees/binarytrees/BinaryTree.java
@@ -1,11 +1,13 @@
 package trees.binarytrees;
 
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 
 public class BinaryTree {
 
 	Node root;
+	int index = 0;
 	
 	public BinaryTree() {
 		this.root = new Node();
@@ -23,6 +25,24 @@ public class BinaryTree {
 		System.out.println(root.data);
 		inorderTraversal(root.right);
 	}
+	
+	public void preorderTraversal(Node root) {
+		if(root == null) {
+			return;
+		}
+		System.out.println(root.data);
+		preorderTraversal(root.left);
+		preorderTraversal(root.right);
+	}
+	
+	public void postorderTraversal(Node root) {
+		if(root == null) {
+			return;
+		}
+		postorderTraversal(root.left);
+		postorderTraversal(root.right);
+		System.out.println(root.data);
+	}
 
 	public void levelOrderTraversal(Node root) {
 		Queue<Node> queue = new LinkedList<Node>();
@@ -38,5 +58,32 @@ public class BinaryTree {
 			}
 		}
 	}
+
+	/*
+	 * Serialization and deserialization using preorder traversal approach
+	 */
+	public void serializationOfBinaryTree(Node root, List<Integer> list) {
+		if(root != null) {
+			list.add(root.data);
+			serializationOfBinaryTree(root.left, list);
+			serializationOfBinaryTree(root.right, list);
+		} else {
+			list.add(-1);
+		}
+	}
 	
+	public Node desrializationOfBinaryTree(List<Integer> list) {
+		if(index == list.size()) {
+			return null;
+		}
+		int currentValue = list.get(index++);
+		if(currentValue == -1) {
+			return null;
+		}
+		Node root = new Node(currentValue);
+		root.left = desrializationOfBinaryTree(list);
+		root.right = desrializationOfBinaryTree(list);
+		return root;
+	}
+
 }
--- ReReiterate/src/trees/binarytrees/BinaryTreesDriver.java
+++ ReReiterate/src/trees/binarytrees/BinaryTreesDriver.java
@@ -1,16 +1,23 @@
 package trees.binarytrees;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BinaryTreesDriver {
 
 	public static void main(String[] args) {
-		BinaryTree binaryTree = new BinaryTree(1);
+		BinaryTree binaryTree = new BinaryTree(10);
 		Node root = binaryTree.root;
-		root.left = new Node(2);
-		root.left.left = new Node(4);
-		root.left.right = new Node(5);
-		root.right = new Node(3);
+		root.left = new Node(20);
+		root.left.left = new Node(30);
+		root.left.left.right = new Node(40);
 		
-		binaryTree.levelOrderTraversal(root);
+		List<Integer> list = new ArrayList<Integer>();
+		
+		binaryTree.serializationOfBinaryTree(root, list);
+		for(int x : list) {
+			System.out.println(x);
+		}
 	
 	}
 
