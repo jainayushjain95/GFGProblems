70ffa4f5eee7c0f5372816cd0a1318bce3d3cda4
Ayush Jain <jainayushjain95@gmail.com>
2020-09-27 14:48:49 +0530

    Probs

--- ReReiterate/src/strings/NaivePatternSearch.java
+++ ReReiterate/src/strings/NaivePatternSearch.java
@@ -3,8 +3,8 @@ package strings;
 public class NaivePatternSearch {
 
 	public static void main(String[] args) {
-		String text = "Geeks for Geeks";
-		String pattern = "eks";
+		String text = "ABCABCDABCD";
+		String pattern = "ABCD";
 		patternSearchA(text, pattern);
 	}
 
@@ -41,6 +41,21 @@ public class NaivePatternSearch {
 		if(m > n) {
 			System.out.println("Not Present");
 			return;
+		}
+		int noOfWindows = n - m + 1;
+		for(int i = 0;i < noOfWindows;) {
+			int j = 0;
+			while(j < m && text.charAt(i + j) == pattern.charAt(j)) {
+				j++;
+			}
+			if(j == pattern.length()) {
+				System.out.println(i);
+			}
+			if(j == 0) {
+				i++;
+			} else {
+				i = i + j;	
+			}
 		}
 	}
 }
