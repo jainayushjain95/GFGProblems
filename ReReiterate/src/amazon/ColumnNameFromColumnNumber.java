6c716b09f918fe56f77fa4e1dafb23fa014e7fc6
Ayush Jain <jainayushjain95@gmail.com>
2020-10-14 09:23:33 +0530

    Probs

--- ReReiterate/src/amazon/ColumnNameFromColumnNumber.java
+++ ReReiterate/src/amazon/ColumnNameFromColumnNumber.java
@@ -1,10 +1,26 @@
 package amazon;
 
+import java.util.Scanner;
+
 public class ColumnNameFromColumnNumber {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		int t = Integer.parseInt(sc.nextLine());
+		while(t != 0) {
+			t--;
+			System.out.println(solve(sc.nextLong()));
+		}
+	}
+	
+	public static String solve(long N) {
+		String solution = "";
+		while(N > 0) {
+			solution = (char)('A' + (N - 1) % 26) + solution;
+			N = (N - 1)/26;
+		}
+		
+		return solution;
 	}
 
 }
