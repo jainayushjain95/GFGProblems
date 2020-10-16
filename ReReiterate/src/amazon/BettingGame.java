74b444b89fcb6cc0c93aac0746be7f1368a8707a
Ayush Jain <jainayushjain95@gmail.com>
2020-10-16 19:18:27 +0530

    Probs

--- ReReiterate/src/amazon/BettingGame.java
+++ ReReiterate/src/amazon/BettingGame.java
@@ -13,11 +13,27 @@ public class BettingGame {
 			t--;
 			sb.append(solve(sc.nextLine()) + "\n");
 		}
+		System.out.println(sb.toString());
 	}
 
 	public static int solve(String S) {
-		int solution = 0;
-		
+		int solution = 4, betAmount = 1, N = S.length();
+		for(int i = 0;i < N; i++) {
+			if(betAmount > solution) {
+				solution = -1;
+				break;
+			} else	if(S.charAt(i) == 'W') {
+				solution = solution + betAmount;
+				betAmount = 1;
+			} else {
+				if(solution < betAmount) {
+					solution = -1;
+					break;
+				}
+				solution = solution - betAmount;
+				betAmount = 2 * betAmount;
+			}
+		}
 		return solution;
 	}
 }
