1b08ccd31199ad6f26e0ade8b39fecae00f6c4f6
Ayush Jain <jainayushjain95@gmail.com>
2020-10-16 15:00:58 +0530

    Probs

--- ReReiterate/src/amazon/CountofStringsThatCanBeFormedUsingabcUnderGivenConstraints.java
+++ ReReiterate/src/amazon/CountofStringsThatCanBeFormedUsingabcUnderGivenConstraints.java
@@ -1,10 +1,69 @@
 package amazon;
 
+import java.util.Scanner;
+
 public class CountofStringsThatCanBeFormedUsingabcUnderGivenConstraints {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		StringBuilder sb = new StringBuilder();
+		int t = Integer.parseInt(sc.nextLine());
+		while(t != 0) {
+			t--;
+			sb.append(solve(Integer.parseInt(sc.nextLine())) + "\n");
+		}
+//		System.out.println(sb.toString());
 
+	}
+
+	public static int solve(int n) {
+		long x = System.currentTimeMillis();
+		int[][][] dp = new int[n + 1][2][3];
+		int solution = solveRecDP(n, 1, 2, dp);
+		long y = System.currentTimeMillis();
+		long a = y - x;
+		
+		x = System.currentTimeMillis();
+		solution = solveRec(n, 1, 2);
+		y = System.currentTimeMillis();
+		long b = y - x;
+		System.out.println(a + ", " + b);
+		return solution;
+	}
+
+	public static int solveRec(int n, int bCount, int cCount) {
+
+		if(bCount < 0 || cCount < 0) {
+			return 0;
+		}
+		if(n == 0) {
+			return 1;
+		}
+		if(bCount == 0 && cCount == 0) {
+			return 1;
+		}
+		
+		int res = solveRec(n - 1, bCount, cCount) + solveRec(n - 1, bCount - 1, cCount) + solveRec(n - 1, bCount, cCount - 1);
+		return res;
+	}
+
+	public static int solveRecDP(int n, int bCount, int cCount, int[][][] dp) {
+
+		if(bCount < 0 || cCount < 0) {
+			return 0;
+		}
+		if(n == 0) {
+			return 1;
+		}
+		if(bCount == 0 && cCount == 0) {
+			return 1;
+		}
+		if(dp[n][bCount][cCount] > 0) {
+			return dp[n][bCount][cCount];
+		}
+		int res = solveRecDP(n - 1, bCount, cCount, dp) + solveRecDP(n - 1, bCount - 1, cCount, dp) + solveRecDP(n - 1, bCount, cCount - 1, dp);
+		dp[n][bCount][cCount] = res;
+		return res;
 	}
 
 }
