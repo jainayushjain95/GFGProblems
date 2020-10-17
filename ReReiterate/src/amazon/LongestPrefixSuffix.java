830c5cfb3bd0e01bdb9a41084e8797106002fae7
Ayush Jain <jainayushjain95@gmail.com>
2020-10-17 11:20:08 +0530

    Probs

---
+++ ReReiterate/src/amazon/LongestPrefixSuffix.java
@@ -0,0 +1,15 @@
+package amazon;
+
+public class LongestPrefixSuffix {
+
+	public static void main(String[] args) {
+		String S = "abab";
+		System.out.println(solve(S));
+	}
+	
+	public static int solve(String S) {
+		int lps = 0;
+		return lps;
+	}
+
+}
