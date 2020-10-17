b79d9d24b7920309934c7f5cf6c483d95c802206
Ayush Jain <jainayushjain95@gmail.com>
2020-10-17 18:35:45 +0530

    Probs

---
+++ ReReiterate/src/amazon/RemoveDuplicatesInASTtring.java
@@ -0,0 +1,30 @@
+package amazon;
+
+public class RemoveDuplicatesInASTtring {
+
+	public static void main(String[] args) {
+		String S = "HappyNewYear";
+		System.out.println(solve(S));
+	}
+	
+	public static String solve(String S) {
+		String output = "";
+		int N = S.length();
+		if(N <= 1) {
+			output = S;
+		} else {
+			StringBuilder sb = new StringBuilder();
+			int left = 0, right = 0;
+			int[] alpha = new int[255];
+			for(int i = 0;i < N; i++) {
+				if(alpha[S.charAt(i)] == 0) {
+					sb.append(S.charAt(i));
+					alpha[S.charAt(i)]++;
+				}
+			}
+			output = sb.toString();
+		}
+		return output;
+	}
+
+}
