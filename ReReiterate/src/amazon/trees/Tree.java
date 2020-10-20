d612d627027ea96fde7b73f3bac6a6921d4aff04
Ayush Jain <jainayushjain95@gmail.com>
2020-10-20 10:11:32 +0530

    Probs

--- ReReiterate/src/amazon/trees/Tree.java
+++ ReReiterate/src/amazon/trees/Tree.java
@@ -2,5 +2,36 @@ package amazon.trees;
 
 public class Tree {
 	Node root;
+
+	public Tree(Node root) {
+		this.root = root;
+	}
+
+	public void inorderTraversal(Node root) {
+		if(root == null) {
+			return;
+		}
+		inorderTraversal(root.left);
+		System.out.println(root.data);
+		inorderTraversal(root.right);
+	}
+	
+	public void postorderTraversal(Node root) {
+		if(root == null) {
+			return;
+		}
+		postorderTraversal(root.left);
+		postorderTraversal(root.right);
+		System.out.println(root.data);
+	}
+	
+	public void preorderTraversal(Node root) {
+		if(root == null) {
+			return;
+		}
+		System.out.println(root.data);
+		preorderTraversal(root.left);
+		preorderTraversal(root.right);
+	}
 	
 }
--- ReReiterate/src/amazon/trees/TreesDriver.java
+++ ReReiterate/src/amazon/trees/TreesDriver.java
@@ -1,9 +1,32 @@
 package amazon.trees;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class TreesDriver {
 
 	public static void main(String[] args) {
-
+		Tree tree = new Tree(new Node(1));
+		Node root = tree.root;
+		getNthLevelPerfectBinaryTree(3, tree);
+		tree.inorderTraversal(root);
 	}
 
+	public static void getNthLevelPerfectBinaryTree(int levels, Tree tree) {
+		Node root = tree.root;
+		int data = 2;
+		Queue<Node> queue = new LinkedList<Node>();
+		queue.add(root);
+		while(levels > 1) {
+			int queueSize = queue.size();
+			for(int i = 0;i < queueSize; i++) {
+				Node node = queue.poll();
+				node.left = new Node(data++);
+				node.right = new Node(data++);
+				queue.add(node.left);
+				queue.add(node.right);
+			}
+			levels--;
+		}
+	}
 }
