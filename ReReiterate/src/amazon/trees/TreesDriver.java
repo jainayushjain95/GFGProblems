8cb308585fa6b4586f3488e212fbb0f34991fa1e
Ayush Jain <jainayushjain95@gmail.com>
2020-10-20 10:15:50 +0530

    Probs

--- ReReiterate/src/amazon/trees/TreesDriver.java
+++ ReReiterate/src/amazon/trees/TreesDriver.java
@@ -29,4 +29,36 @@ public class TreesDriver {
 			levels--;
 		}
 	}
+	
+	public void getTree(Tree tree) {
+		Node root = tree.root;
+		root.left = new Node(2);
+		root.right = new Node(3);
+
+		root.left.left = new Node(4);
+		root.left.right = new Node(5);
+
+		root.right.left = new Node(6);
+		root.right.right = new Node(7);
+
+		root.left.left.left = new Node(8);
+		root.left.left.right = new Node(9);
+
+		root.left.right.left = new Node(10);
+		root.left.right.right = new Node(11);
+
+		root.right.left.left = new Node(12);
+		root.right.left.right = new Node(13);
+		root.right.right.left = new Node(14);
+		root.right.right.right = new Node(15);
+
+		root.left.left.right.left = new Node(16);
+		root.left.left.right.right = new Node(17);
+
+		root.right.left.left.left = new Node(18);
+		root.right.left.left.right = new Node(19);
+
+		root.right.right.left.right = new Node(20);
+		root.right.right.right.left = new Node(21);
+	}
 }
