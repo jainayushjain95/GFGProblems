90435d7b8fc4e4953cf300883c0493bb37bb75ba
jainsaab <jain.ayush@infoedge.com>
2020-12-12 15:13:52 +0530

    Recursion Problems

--- DSAlgoPrac/src/recursion/gfg/RecursionProblems.java
+++ DSAlgoPrac/src/recursion/gfg/RecursionProblems.java
@@ -3,8 +3,8 @@ package recursion.gfg;
 public class RecursionProblems {
 
 	public static void main(String[] args) {
-		String S = "geeks";
-		System.out.println(isPalindrome(S, 0, S.length() - 1));
+		String S = "abc";
+		printSubsets(S, "", 0);
 	}
 
 	public static boolean isPalindrome(String S, int beginIndex, int endIndex) {
@@ -13,5 +13,32 @@ public class RecursionProblems {
 		}
 		
 		return S.charAt(beginIndex) == S.charAt(endIndex) && isPalindrome(S, beginIndex + 1, endIndex - 1);
+	}
+	
+	public static int sumOfDigits(int n) {
+		if(n < 10) {
+			return n;
+		}
+		
+		return (n % 10) + sumOfDigits(n/10);
+	}
+	
+	public static int ropeCuttingProblem(int n, int a, int b, int c) {
+		if(n == 0) {
+			return 0;
+		}
+		if(n < 0) {
+			return -1;
+		}
+		return 1 + Math.max(ropeCuttingProblem(n - a, a, b, c), Math.max(ropeCuttingProblem(n - b, a, b, c), ropeCuttingProblem(n - c, a, b, c)));
+	}
+	
+	public static void printSubsets(String S, String curr, int index) {
+		if(index == S.length()) {
+			System.out.println(curr);
+			return;
+		}
+		printSubsets(S, curr, index + 1);
+		printSubsets(S, curr + S.charAt(index), index + 1);
 	}
 }
